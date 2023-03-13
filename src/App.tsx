b7a08270d1929e01900485277c0b6835c671e413
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-03-13 02:55:14 -0300

    ✨ feat: applying global styles and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './styles/global';
+import light from './styles/themes/light';
+
 export const App: React.FC = () => {
   return (
-    <h1>App</h1>
+    <ThemeProvider theme={light}>
+      <GlobalStyles />
+      <h1>App</h1>
+    </ThemeProvider>
   );
 };
