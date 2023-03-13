240e90849276fe3c795f49013535f44e04697f34
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-03-13 02:53:14 -0300

    🐳 chore: modified port

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import { App } from './App'
+import ReactDOM from 'react-dom';
+import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
